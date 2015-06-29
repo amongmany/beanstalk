55e1d6a7516abfa4f6a71e545844f1e94e043548
amongmany <danny@amongmany.com>
2015-06-29 13:49:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beanstalk::Application.config.secret_key_base = 'ccc44000e8a331ff5c53cb25a62f52f96b330e16700310f5d9dd9323933559160fa6c09adb5b1d67bc1a958e4eb9d4aa5172d8b18cb8f9ad291b6f3126e63c27'
